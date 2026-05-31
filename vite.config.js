4dc7461f340ef131aaccd27e079d5bcda464d6c8
Kawsar <diu.kawsar@gmail.com>
2026-05-31 11:28:29 +0600

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/svg_master_pro/',
   plugins: [react()],
   build: {
     rollupOptions: {
